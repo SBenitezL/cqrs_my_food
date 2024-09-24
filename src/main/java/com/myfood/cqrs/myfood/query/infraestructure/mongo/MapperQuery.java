157fff0546c39fdf3c77439dd966c39baa8d12c1
sBenitezL <sbenitez1607@gmail.com>
2024-09-24 17:16:20 -0500

    Fix: rename query controller mapper to MapperQuery

--- src/main/java/com/myfood/cqrs/myfood/query/infraestructure/mongo/MapperQuery.java
+++ src/main/java/com/myfood/cqrs/myfood/query/infraestructure/mongo/MapperQuery.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class mapper {
+public class MapperQuery {
     @Bean
     public ModelMapper createMapper() {
         return new ModelMapper();
